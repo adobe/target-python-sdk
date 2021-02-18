48a3181717d08a9a7df23c7f24aa67231292ffac
Manjiri Tapaswi <tapaswi@adobe.com>
2021-02-18 14:34:14 -0800

    Removed get_offers call from test_attributes_provider

--- target_tools/tests/test_attributes_provider.py
+++ target_tools/tests/test_attributes_provider.py
@@ -28,9 +28,36 @@ class TestAttributesProvider(unittest.TestCase):
             'client': "someClientId",
             'organization_id': "someOrgId"
         }
-        self.get_offers_options = {
-            'request': {
-
+        self.get_attributes_options = {
+            "request": {
+                "request_id": "2e401dcc43ea437b8cb6d178d49303e2",
+                "id": {
+                    "tnt_id": "338e3c1e51f7416a8e1ccba4f81acea0.28_0",
+                    "marketing_cloud_visitor_id": "07327024324407615852294135870030620007"
+                },
+                "context": {
+                    "channel": ChannelType.WEB,
+                    "address": {
+                        "url": "http://adobe.com"
+                    },
+                    "user_agent":
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0) Gecko/20100101 Firefox/73.0",
+                    "beacon": False
+                },
+                "experience_cloud": {
+                    "analytics": {
+                        "supplemental_data_id": "37D04BA8ED0E2962-71B165AC17259331",
+                        "logging": "server_side"
+                    }
+                },
+                "prefetch": {
+                    "mboxes": [
+                        {
+                            "index": 2,
+                            "name": "feature-flag-a"
+                        }
+                    ]
+                }
             }
         }
         self.client = TargetClient.create(client_options)
@@ -39,11 +66,10 @@ class TestAttributesProvider(unittest.TestCase):
     @responses.activate
     def test_has_appropriate_method_calls(self):
         setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_offers_options)
+        opts = deepcopy(self.get_attributes_options)
         opts['request'] = create_delivery_request(opts['request'])
-        result = self.client.get_offers(opts)
 
-        feature = AttributesProvider(result['response'])
+        feature = AttributesProvider(opts['response'])
 
         self.assertTrue(callable(feature.get_value))
         self.assertTrue(callable(feature.as_object))
@@ -55,11 +81,11 @@ class TestAttributesProvider(unittest.TestCase):
     @responses.activate
     def test_gets_value_for_single_mbox(self):
         setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_offers_options)
+        opts = deepcopy(self.get_attributes_options)
         opts['request'] = create_delivery_request(opts['request'])
         result = self.client.get_offers(opts)
 
-        feature_a = AttributesProvider(result['response'])
+        feature_a = AttributesProvider(opts['response'])
 
         self.assertEqual(
             feature_a.get_value(
@@ -81,7 +107,7 @@ class TestAttributesProvider(unittest.TestCase):
                 "customer_feedback_value"),
             10)
 
-        feature_b = AttributesProvider(result['response'])
+        feature_b = AttributesProvider(opts['response'])
 
         self.assertEqual(
             feature_b.get_value(
@@ -106,10 +132,9 @@ class TestAttributesProvider(unittest.TestCase):
     @responses.activate
     def test_gets_value_for_multiple_mboxes_at_once(self):
         setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_offers_options)
+        opts = deepcopy(self.get_attributes_options)
         opts['request'] = create_delivery_request(opts['request'])
-        result = self.client.get_offers(opts)
-        features = AttributesProvider(result['response'])
+        features = AttributesProvider(opts['response'])
 
         self.assertEqual(
             features.get_value(
@@ -151,10 +176,9 @@ class TestAttributesProvider(unittest.TestCase):
     @responses.activate
     def test_gets_as_object(self):
         setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_offers_options)
+        opts = deepcopy(self.get_attributes_options)
         opts['request'] = create_delivery_request(opts['request'])
-        result = self.client.get_offers(opts)
-        features = AttributesProvider(result['response'])
+        features = AttributesProvider(opts['response'])
 
         self.assertDictEqual(features.as_object("feature-flag-a"), {
             'payment_experience': "legacy",
@@ -203,10 +227,9 @@ class TestAttributesProvider(unittest.TestCase):
     @responses.activate
     def test_throws_an_error_if_an_attribute_does_not_exist(self):
         setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_offers_options)
+        opts = deepcopy(self.get_attributes_options)
         opts['request'] = create_delivery_request(opts['request'])
-        result = self.client.get_offers(opts)
-        features = AttributesProvider(result['response'])
+        features = AttributesProvider(opts['response'])
 
         with self.assertRaises(Exception) as context:
             features.get_value("feature-flag-a", "my_property_name")

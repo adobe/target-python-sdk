1c3f3ffb66b2fae6eef74077e2b0e4dad6717d9d
Manjiri Tapaswi <tapaswi@adobe.com>
2021-02-18 16:10:26 -0800

    Use Mock Response in AttributesProvider

--- target_tools/tests/test_attributes_provider.py
+++ target_tools/tests/test_attributes_provider.py
@@ -10,80 +10,127 @@
 
 """Test cases for attributes_provider.py"""
 import unittest
-from copy import deepcopy
-from urllib3_mock import Responses
-from delivery_api_client import ChannelType
-from target_python_sdk import TargetClient
-from target_python_sdk.tests.delivery_api_mock import setup_mock
-from target_python_sdk.tests.delivery_request_setup import create_delivery_request
 from target_tools.attributes_provider import AttributesProvider
 
-responses = Responses('requests.packages.urllib3')
-
-class TestAttributesProvider(unittest.TestCase):
-
-    def setUp(self):
-        client_options = {
-            'client': "someClientId",
-            'organization_id': "someOrgId"
+TARGET_DELIVERY_RESPONSE = {
+    "visitor_state": {
+        "65453EA95A70434F0A495D34@AdobeOrg": {
+            "sdid": {
+                "supplemental_data_id_current": "37D04BA8ED0E2962-71B165AC17259331",
+                "supplemental_data_id_current_consumed": {
+                    "payload:target-global-mbox": True
+                },
+                "supplemental_data_id_last_consumed": {}
+            }
         }
-        self.get_attributes_options = {
-            "request": {
-                "request_id": "2e401dcc43ea437b8cb6d178d49303e2",
-                "id": {
-                    "tnt_id": "338e3c1e51f7416a8e1ccba4f81acea0.28_0",
-                    "marketing_cloud_visitor_id": "07327024324407615852294135870030620007"
-                },
-                "context": {
-                    "channel": ChannelType.WEB,
-                    "address": {
-                        "url": "http://adobe.com"
-                    },
-                    "user_agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0) Gecko/20100101 Firefox/73.0",
-                    "beacon": False
-                },
-                "experience_cloud": {
-                    "analytics": {
-                        "supplemental_data_id": "37D04BA8ED0E2962-71B165AC17259331",
-                        "logging": "server_side"
-                    }
-                },
-                "prefetch": {
-                    "mboxes": [
+    },
+    "request": {
+        "request_id": "2e401dcc43ea437b8cb6d178d49303e2",
+        "id": {
+            "tnt_id": "338e3c1e51f7416a8e1ccba4f81acea0.28_0",
+            "marketing_cloud_visitor_id": "07327024324407615852294135870030620007"
+        },
+        "context": {
+            "channel": "web",
+            "address": {
+                "url": "http://adobe.com"
+            },
+            "user_agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0) Gecko/20100101 Firefox/73.0",
+            "beacon": False
+        },
+        "experience_cloud": {
+            "analytics": {
+                "supplemental_data_id": "37D04BA8ED0E2962-71B165AC17259331",
+                "logging": "server_side"
+            }
+        },
+        "prefetch": {
+            "mboxes": [
+                {
+                    "index": 2,
+                    "name": "feature-flag-a"
+                }
+            ]
+        }
+    },
+    "target_cookie": {
+        "name": "mbox",
+        "value":
+        "session#dummy_session#1583190919|PC#338e3c1e51f7416a8e1ccba4f81acea0.28_0#1646433859",
+        "max_age": 63244801
+    },
+    "target_location_hint_cookie": {
+        "name": "mboxEdgeCluster",
+        "value": "28",
+        "max_age": 1860
+    },
+    "response": {
+        "status": 200,
+        "request_id": "2e401dcc43ea437b8cb6d178d49303e2",
+        "id": {
+            "tnt_id": "338e3c1e51f7416a8e1ccba4f81acea0.28_0",
+            "marketing_cloud_visitor_id": "07327024324407615852294135870030620007"
+        },
+        "client": "adobesummit2018",
+        "edge_host": "mboxedge28.tt.omtrdc.net",
+        "prefetch": {
+            "mboxes": [
+                {
+                    "index": 1,
+                    "name": "feature-flag-a",
+                    "options": [
                         {
-                            "index": 2,
-                            "name": "feature-flag-a"
+                            "content_type": "json",
+                            "content": {
+                                "payment_experience": "legacy",
+                                "show_feature_x": False,
+                                "payment_gateway_version": 2.3,
+                                "customer_feedback_value": 10
+                            },
+                            "event_token":
+                            "8MDICvd7bsTPYn79fLBNQmqipfsIHvVzTQxHolz2IpSCnQ9Y9OaLL2gsdrWQTvE54PwSz67rmXWmSnkXpSSS2Q=="
                         }
                     ]
                 }
-            }
+            ]
+        },
+        "execute": {
+            "mboxes": [
+                {
+                    "index": 1,
+                    "name": "feature-flag-b",
+                    "options": [
+                        {
+                            "content_type": "json",
+                            "content": {
+                                "purchase_experience": "beta2",
+                                "show_feature_y": True,
+                                "cart_version": 1.3,
+                                "customer_survey_value": 102
+                            }
+                        }
+                    ]
+                }
+            ]
         }
-        self.client = TargetClient.create(client_options)
+    }
+}
 
+class TestAttributesProvider(unittest.TestCase):
 
-    @responses.activate
     def test_has_appropriate_method_calls(self):
-        setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_attributes_options)
-        opts['request'] = create_delivery_request(opts['request'])
-
-        feature = AttributesProvider(opts['response'])
+        feature = AttributesProvider(TARGET_DELIVERY_RESPONSE)
 
         self.assertTrue(callable(feature.get_value))
         self.assertTrue(callable(feature.as_object))
         self.assertTrue(callable(feature.to_json))
         self.assertTrue(callable(feature.get_response))
 
-        self.assertDictEqual(feature.get_response(), opts['response'])
+        self.assertDictEqual(feature.get_response(), TARGET_DELIVERY_RESPONSE)
 
-    @responses.activate
     def test_gets_value_for_single_mbox(self):
-        setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_attributes_options)
-        opts['request'] = create_delivery_request(opts['request'])
-
-        feature_a = AttributesProvider(opts['response'])
+        feature_a = AttributesProvider(TARGET_DELIVERY_RESPONSE)
 
         self.assertEqual(
             feature_a.get_value(
@@ -105,7 +152,7 @@ class TestAttributesProvider(unittest.TestCase):
                 "customer_feedback_value"),
             10)
 
-        feature_b = AttributesProvider(opts['response'])
+        feature_b = AttributesProvider(TARGET_DELIVERY_RESPONSE)
 
         self.assertEqual(
             feature_b.get_value(
@@ -127,12 +174,8 @@ class TestAttributesProvider(unittest.TestCase):
                 "customer_survey_value"),
             102)
 
-    @responses.activate
     def test_gets_value_for_multiple_mboxes_at_once(self):
-        setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_attributes_options)
-        opts['request'] = create_delivery_request(opts['request'])
-        features = AttributesProvider(opts['response'])
+        features = AttributesProvider(TARGET_DELIVERY_RESPONSE)
 
         self.assertEqual(
             features.get_value(
@@ -171,12 +214,8 @@ class TestAttributesProvider(unittest.TestCase):
                 "customer_survey_value"),
             102)
 
-    @responses.activate
     def test_gets_as_object(self):
-        setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_attributes_options)
-        opts['request'] = create_delivery_request(opts['request'])
-        features = AttributesProvider(opts['response'])
+        features = AttributesProvider(TARGET_DELIVERY_RESPONSE)
 
         self.assertDictEqual(features.as_object("feature-flag-a"), {
             'payment_experience': "legacy",
@@ -222,12 +261,8 @@ class TestAttributesProvider(unittest.TestCase):
             }
         })
 
-    @responses.activate
     def test_throws_an_error_if_an_attribute_does_not_exist(self):
-        setup_mock('attributes_provider', responses)
-        opts = deepcopy(self.get_attributes_options)
-        opts['request'] = create_delivery_request(opts['request'])
-        features = AttributesProvider(opts['response'])
+        features = AttributesProvider(TARGET_DELIVERY_RESPONSE)
 
         with self.assertRaises(Exception) as context:
             features.get_value("feature-flag-a", "my_property_name")

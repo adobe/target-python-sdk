c5bb78a82977fa39346cdbb65c0dfa36ad22479b
Manjiri Tapaswi <tapaswi@adobe.com>
2021-02-19 14:07:56 -0800

    TNT-38926: Add SDK Events

--- target_tools/event_provider.py
+++ target_tools/event_provider.py
@@ -10,15 +10,51 @@
 
 """Event Provider"""
 
+class TargetEvent:
+    def __init__(self, event_type, payload={}):
+        self[event_type] = event_type
+        for key in payload.keys():
+            self[key] = payload.get(key)
 
 class EventProvider:
     """EventProvider"""
 
-    def __init__(self, events=None):
-        print(events)
+    def __init__(self, events={}):
+        """
+        :param events: (dict.<str, callable>) An object with event name keys and callback
+            function values, optional
+        """
+        self.subscriptions = {}
+        self.subscription_count = 0
+        for event_name in events.keys():
+            self.subscribe(event_name, events.get(event_name))
 
-    def emit(self, event_name, payload=None):
-        """Emits events"""
+    def subscribe(self, event_name, callback_func):
+        """Subscribe to events
+        :param event_name: (str) Event name, required
+        callback_func: (callable) Callback function, required
+        :return: (str)
+        """
+        self.subscription_count += 1
+        if not self.subscriptions.get(event_name):
+            self.subscriptions[event_name] = {}
+        
+        self.subscriptions[event_name][self.subscription_count] = callback_func
+        return "{}:{}".format(event_name, self.subscription_count)
+ 
+    def unsubscribe(self, id):
+        """Unsubscribe from events
+        :param id: (str) Event Id, required
+        """
+        event_name, event_id = id.split(":")
+        if self.subscriptions.get(event_name):
+            self.subscriptions[event_name][event_id]=None
 
-    def subscribe(self):
-        """Subscribe to events"""
+    def emit(self, event_name, payload={}):
+        """Emits events
+        :param event_name: (str) Event name, required
+        payload: (dict) Payload, optional
+        """
+        subscribed = self.subscriptions.get(event_name) or []
+        for key, subscriber in subscribed.items():
+            subscriber(TargetEvent(event_name, payload))
